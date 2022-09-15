fdb23b7717b843d85c1eb8e5191b9491abf8c05e
rahul-1230 <72060389+rahul-1230@users.noreply.github.com>
2022-09-15 09:26:50 +0530

    third

--- backend/util.js
+++ backend/util.js
@@ -30,14 +30,8 @@ const isAuth = (req, res, next) => {
   }
 }
 
-const isAdmin = (req, res, next) => {
-  console.log(req.user)
-  if (req.user && req.user.isAdmin) {
-    return next();
-  }
-  return res.status(401).send({ msg: 'Admin Token is not valid.' })
-}
+
 
 export {
-  getToken, isAuth, isAdmin
+  getToken, isAuth
 }
